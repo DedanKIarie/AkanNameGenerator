0357f367f6399631c2fef6986963c2db24b1e722
Dedan Kiarie <dedan.kiarie@student.moringaschool.com>
2025-01-24 15:25:24 +0300

    Fixed the formular

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
 function Clicked() {
     event.preventDefault(); 
-    let DD = parseInt(document.getElementById("DD").value, 10);
-    let MM = parseInt(document.getElementById("MM").value, 10); 
-    let Year = parseInt(document.getElementById("YY").value, 10); 
+    let DD = parseInt(document.getElementById("DD").value);
+    let MM = parseInt(document.getElementById("MM").value); 
+    let Year = parseInt(document.getElementById("YY").value); 
     let MaleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     let FemaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua" ,"Ama"];
     let Male = document.getElementById("Male");
     let Female = document.getElementById("Female");
   
-    let CC = Math.floor(Year /100);
-    let YY = Year%100;
+    let CC = (Year /100);
+    let YY = Math.ceil(Year%100);
 
 
-    let day = Math.floor(
-        (((CC/4)-2*CC-1)+((5*YY)/4)+((26*(MM+1))/10)+DD)%7
-    );
+    let day = Math.ceil((((CC/4)-2*CC-1)+(Math.ceil(5*YY/4))+(Math.ceil(26*(MM+1)/10))+DD)%7);
 
     if (Male.checked) {
-        console.log(MaleNames[day]);
-    } else 
-     if (Female.checked) {
-        console.log(FemaleNames[day]);
+        document.getElementById("showndata").innerHTML = "Your Akan Name is " + (MaleNames[day]);
+    } else if (Female.checked) {
+        document.getElementById("showndata").innerHTML = "Your Akan Name is " + (FemaleNames[day]);
     }
     
 }
